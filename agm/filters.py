f030e7164ee8eaed9208e5100d511825cc0958ec
shafiq <ssekittoshafiq@gmail.com>
2025-09-11 13:17:12 +0300

    asd

--- agm/filters.py
+++ agm/filters.py
@@ -31,11 +31,31 @@ class MemberFilter(django_filters.FilterSet):
         label="Status",
         widget=forms.Select(attrs={"class": "form-control"})
     )
+    name = django_filters.ModelMultipleChoiceFilter(
+        queryset=Member.objects.all(),
+        label="Name",
+        method="filter_by_full_name",
+        widget=forms.SelectMultiple(
+            attrs={"class": "form-control js-example-basic-multiple-name"}
+        )
+    )
 
     class Meta:
         model = Member
         fields = [
             "region",
-            "designation","status",
+            "designation","status","name",
             "gender","id_range",
         ]  # Add all fields you want to filter on
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Show "First Last" in the dropdown
+        self.filters['name'].field.label_from_instance = (
+            lambda obj: f"{obj.first_name} {obj.last_name}"
+        )
+
+    def filter_by_full_name(self, queryset, name, value):
+        """Filter by selected member IDs, while keeping other filters."""
+        if value:
+            queryset = queryset.filter(id__in=[v.id for v in value])
+        return queryset
